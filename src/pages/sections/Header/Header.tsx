e0d1f82f36f711fcdfe9dcba255d0f826c4e085d
Rodolphe Lemasquerier <rodolphe.lemasquerier.ext@skello.io>
2023-03-07 13:38:02 +0100

    chore: remove next images and default content

--- src/pages/sections/Header/Header.tsx
+++ src/pages/sections/Header/Header.tsx
@@ -8,16 +8,7 @@ export const Header = () => {
         <Image className={styles.logo} src="/logo.svg" alt="Next.js Logo" width={56} height={56} priority />
         <Image className={styles.logo} src="/nemeos.svg" alt="Next.js Logo" width={170} height={24} priority />
       </div>
-      <div>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
-        </a>
-      </div>
+      <div></div>
     </div>
   );
 };
