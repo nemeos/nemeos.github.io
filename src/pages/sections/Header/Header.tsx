99a3fe35570d1a31bb675ac516ecef62ea4b45f1
Rodolphe Lemasquerier <rodolphe.lemasquerier.ext@skello.io>
2023-03-07 13:29:21 +0100

    feat: nemeos logo

--- src/pages/sections/Header/Header.tsx
+++ src/pages/sections/Header/Header.tsx
@@ -4,10 +4,10 @@ import styles from '@/styles/Home.module.css';
 export const Header = () => {
   return (
     <div className={styles.description}>
-      <p>
-        Get started by editing&nbsp;
-        <code className={styles.code}>src/pages/index.tsx</code>
-      </p>
+      <div>
+        <Image className={styles.logo} src="/logo.svg" alt="Next.js Logo" width={56} height={56} priority />
+        <Image className={styles.logo} src="/nemeos.svg" alt="Next.js Logo" width={170} height={24} priority />
+      </div>
       <div>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
