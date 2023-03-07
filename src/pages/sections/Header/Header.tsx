b7590f4afbf0044625f3e6fbfb92c8eeb299b8c3
Rodolphe Lemasquerier <rodolphe.lemasquerier.ext@skello.io>
2023-03-07 13:49:49 +0100

    feat: improve logo

--- src/pages/sections/Header/Header.tsx
+++ src/pages/sections/Header/Header.tsx
@@ -1,14 +1,12 @@
 import Image from 'next/image';
-import styles from '@/styles/Home.module.css';
 
 export const Header = () => {
   return (
-    <div className={styles.description}>
-      <div>
-        <Image className={styles.logo} src="/logo.svg" alt="Next.js Logo" width={56} height={56} priority />
-        <Image className={styles.logo} src="/nemeos.svg" alt="Next.js Logo" width={170} height={24} priority />
+    <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Image src="/logo.svg" alt="Nemeos Logo" width={56} height={56} style={{ marginRight: 16 }} priority />
+        <Image src="/nemeos.svg" alt="Nemeos" width={170} height={24} priority />
       </div>
-      <div></div>
     </div>
   );
 };
