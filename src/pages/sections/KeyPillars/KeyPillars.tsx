4ae68fec40bb6eaaf5daf5839de29ea943abf514
Rodolphe Lemasquerier <rodolphe.lemasquerier.ext@skello.io>
2023-03-12 14:27:20 +0100

    feat: key pillars first part

--- src/pages/sections/KeyPillars/KeyPillars.tsx
+++ src/pages/sections/KeyPillars/KeyPillars.tsx
@@ -1,4 +1,5 @@
 import { Text } from '@/components/Text';
+import Image from 'next/image';
 
 export const KeyPillars = () => {
   return (
@@ -20,6 +21,13 @@ export const KeyPillars = () => {
           Our objective is to allow anyone to operate, build or integrate real-life financial services for web3
           ecosystem. For example, a user can use our solution to buy a NFT with credit.
         </Text>
+      </div>
+      <div style={{ marginTop: 60 }}>
+        <div style={{ display: 'flex', flexDirection: 'column', width: 300 }}>
+          <Image style={{ marginBottom: 32 }} src="/hand-coin.svg" alt="Nemeos" width={80} height={80} priority />
+          <Text style={{ fontSize: 24, fontWeight: 700, marginBottom: 32 }}>Key On-chain credit infrastructure</Text>
+          <Text>Credit layer allowing financing, asset scoring and collateral management.</Text>
+        </div>
       </div>
     </div>
   );
