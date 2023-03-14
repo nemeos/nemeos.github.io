cbd6d8eedacf0ada13387fa4c3f417082ff045ff
Rodolphe Lemasquerier <rodolphe.lemasquerier.ext@skello.io>
2023-03-14 13:24:21 +0100

    feat: rest of key pillars

--- src/pages/sections/KeyPillars/KeyPillars.tsx
+++ src/pages/sections/KeyPillars/KeyPillars.tsx
@@ -13,6 +13,7 @@ export const KeyPillars = () => {
         alignItems: 'flex-start',
         zIndex: 2,
         marginTop: 300,
+        marginBottom: 100,
       }}
     >
       <Text style={{ fontSize: 40, fontWeight: 700 }}>Key pillars</Text>
@@ -22,11 +23,31 @@ export const KeyPillars = () => {
           ecosystem. For example, a user can use our solution to buy a NFT with credit.
         </Text>
       </div>
-      <div style={{ marginTop: 60 }}>
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          flexDirection: 'row',
+          marginTop: 60,
+          paddingRight: 80,
+        }}
+      >
         <div style={{ display: 'flex', flexDirection: 'column', width: 300 }}>
           <Image style={{ marginBottom: 32 }} src="/hand-coin.svg" alt="Nemeos" width={80} height={80} priority />
           <Text style={{ fontSize: 24, fontWeight: 700, marginBottom: 32 }}>Key On-chain credit infrastructure</Text>
           <Text>Credit layer allowing financing, asset scoring and collateral management.</Text>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', width: 300 }}>
+          <Image style={{ marginBottom: 32 }} src="/coin-bundle.svg" alt="Nemeos" width={80} height={80} priority />
+          <Text style={{ fontSize: 24, fontWeight: 700, marginBottom: 32 }}>Credit products fabric</Text>
+          <Text>Creation of any financing products (BNPL, amortizing loan, leasing, etc.) or rental services.</Text>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', width: 300 }}>
+          <Image style={{ marginBottom: 32 }} src="/wallet-check.svg" alt="Nemeos" width={80} height={80} priority />
+          <Text style={{ fontSize: 24, fontWeight: 700, marginBottom: 32 }}>Support web3 adoption</Text>
+          <Text>Go beyond existing DeFi protocols, and bring financial services with real-life use cases to web3.</Text>
         </div>
       </div>
     </div>
