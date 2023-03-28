f60f4f9d2e47a63d5c59fbd1722b4c1e4055a34b
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2023-03-28 23:59:49 +0200

    docs: deploy from github

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -18,7 +18,7 @@ export const Hero = () => {
           </Text>
           <Text style={{ fontSize: 24, color: '#fff', marginBottom: 80 }}>
             We contribute to the development of new inclusive digital economies by building the leading on-chain credit
-            infrastructure for web3 asset.
+            infrastructure for web3 assets.
           </Text>
           <Link href="mailto:francois@nemeos.finance" passHref>
             <Button width={230}>Get in touch</Button>
