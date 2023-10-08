aedaf97cbf9eb61bbadaef1b11cb60bbb734da9e
rigwild <me@rigwild.dev>
2023-10-08 22:22:22 +0200

    Update roadmap

--- src/sections/Roadmap/Roadmap.tsx
+++ src/sections/Roadmap/Roadmap.tsx
@@ -16,7 +16,7 @@ export const RoadMap = () => {
     >
       <Text style={{ fontWeight: 700, fontSize: 40 }}>Our Road map</Text>
       <Text style={{ fontWeight: 700, color: 'rgba(0, 240, 255, 1)', fontSize: 24 }}>
-        Product should be live in Q3.
+        Product should be live in Q4.
       </Text>
       <div
         style={{
@@ -42,7 +42,7 @@ export const RoadMap = () => {
           <Text style={{ marginBottom: 8 }}>Testnet deployment</Text>
         </Card>
         <Card>
-          <Text style={{ fontWeight: 700, fontSize: 16, color: '#000066', marginBottom: 8 }}>Q2 2023</Text>
+          <Text style={{ fontWeight: 700, fontSize: 16, color: '#000066', marginBottom: 8 }}>Q4 2023</Text>
           <Text style={{ fontWeight: 700, fontSize: 20, marginBottom: 8 }}>MVP Deployment</Text>
           <Text style={{ marginBottom: 8 }}>Technical audit</Text>
           <Text style={{ marginBottom: 8 }}>First partnerships with NFT projects</Text>
